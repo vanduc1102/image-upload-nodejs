92d2508ecf7c1d394350d0bcde7b069beda35c63
Duc NGUYEN <vanducld@gmail.com>
2015-01-31 19:23:00 +0700

    you can add image, and see review.

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+var app = angular.module('app', []);
+app.controller('MainCtrl', function ($scope, fileReader) {
+	$scope.getFile = function () {
+        fileReader.readAsDataUrl($scope.file, $scope).then(function(result) {
+		   $scope.imageSrc = result;
+		});
+    };
+});
+app.factory("fileReader",["$q", "$log",function ($q, $log) {
+	var onLoad = function(reader, deferred, scope) {
+		return function () {
+			scope.$apply(function () {
+				deferred.resolve(reader.result);
+			});
+		};
+	};
+
+	var onError = function (reader, deferred, scope) {
+		return function () {
+			scope.$apply(function () {
+				deferred.reject(reader.result);
+			});
+		};
+	};
+
+	var onProgress = function(reader, scope) {
+		return function (event) {
+			scope.$broadcast("fileProgress",
+				{
+					total: event.total,
+					loaded: event.loaded
+				});
+		};
+	};
+
+	var getReader = function(deferred, scope) {
+		var reader = new FileReader();
+		reader.onload = onLoad(reader, deferred, scope);
+		reader.onerror = onError(reader, deferred, scope);
+		reader.onprogress = onProgress(reader, scope);
+		return reader;
+	};
+
+	var readAsDataURL = function (file, scope) {
+		var deferred = $q.defer();
+		 
+		var reader = getReader(deferred, scope);         
+		reader.readAsDataURL(file);
+		 
+		return deferred.promise;
+	};
+
+	return {
+		readAsDataUrl: readAsDataURL  
+	};
+}]);
+app.directive("ngFileSelect",function(){    
+  return {
+    link: function($scope,el){          
+      el.bind("change", function(e){          
+        $scope.file = (e.srcElement || e.target).files[0];
+        $scope.getFile();
+      });          
+    }        
+  }
+});
