305c743b68b8028e3710dad3bfb7d8806e81081f
DucNguyen <vanducld@gmail.com>
2016-11-08 00:13:36 +0700

    able to upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,88 +1,18 @@
 var app = angular.module('app', []);
-app.controller('MainCtrl', function ($scope, fileReader) {
+app.controller('MainCtrl', function ($http ,$scope) {
 	$scope.images=[];
-	
-	$scope.getFile = function () {
-        // fileReader.readManyFileAsDataURL($scope.files, $scope).then(function(imageDataURLs) {
-			// for(var i = 0; i < imageDataURLs.length ; i ++){
-				// var image ={
-					// "url":imageDataURLs[i]
-				// };
-				// $scope.images.push(image);
-			// }    
-			
-		// });
-		for(var i = 0 ; i < $scope.files.length; i ++){
-			var file = $scope.files[i];
-			fileReader.readOneFileAsDataURL(file, $scope).then(function(imageDataURL) {
-				var image ={
-					"url":imageDataURL
-				};
-				$scope.images.push(image);		
-			});
-		}
-    };
-});
-app.factory("fileReader",["$q", "$log",function ($q, $log) {
-	var onLoad = function(reader, deferred, scope) {
-		return function () {
-			scope.$apply(function () {
-				deferred.resolve(reader.result);
-			});
-		};
-	};
-
-	var onError = function (reader, deferred, scope) {
-		return function () {
-			scope.$apply(function () {
-				deferred.reject(reader.result);
-			});
-		};
-	};
-
-	var onProgress = function(reader, scope) {
-		return function (event) {
-			scope.$broadcast("fileProgress",
-				{
-					total: event.total,
-					loaded: event.loaded
-				});
-		};
-	};
-
-	var getReader = function(deferred, scope) {
-		var reader = new FileReader();
-		reader.onload = onLoad(reader, deferred, scope);
-		reader.onerror = onError(reader, deferred, scope);
-		reader.onprogress = onProgress(reader, scope);
-		return reader;
-	};
-
-	var readOneFileAsDataURL= function (file, scope) {
-		var deferred = $q.defer();
-		var reader = getReader(deferred, scope);         
-		reader.readAsDataURL(file);
-		return deferred.promise;
-	};
-	var readManyFileAsDataURL = function(files, scope){
-		var promises=[];
-		for(var i = 0 ; i < files.length; i ++){
-			promises.push(readOneFileAsDataURL(files[i], scope));
-		}
-		return $q.all(promises);
-	};
-	return {
-		readManyFileAsDataURL: readManyFileAsDataURL,
-		readOneFileAsDataURL : readOneFileAsDataURL
-	};
-}]);
-app.directive("ngFileSelect",function(){    
-  return {
-    link: function($scope,el){          
-      el.bind("change", function(e){          
-        $scope.files = (e.srcElement || e.target).files;
-        $scope.getFile();
-      });          
-    }        
-  }
+    $scope.onUpload = function($event){
+    	$http.post('/upload', {
+    		files: $scope.files
+    	},{
+    		headers:{
+    			"content-type": 'multipart/form-data; charset=utf-8',
+    			"accept":'application/json, text/javascript'
+    		}
+    	}).then(function(response){
+    		console.log(response);
+    	},function(error){
+    		console.log(error);
+    	});
+    }
 });
--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var fs = require('fs');
+var busboy = require('connect-busboy'); 
 var http = require('http');
+const path = require('path');
+var uploadedDir = __dirname + path.sep + 'uploaded'+ path.sep;
+
 app.use(express.static(__dirname + '/public'));
+app.use(busboy());
+
+app.route('/upload')
+.post(function (req, res, next) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+        if (!fs.existsSync(uploadedDir)){
+		    fs.mkdirSync(uploadedDir);
+		}
+        //Path where image will be uploaded
+        fstream = fs.createWriteStream(uploadedDir + (new Date()).getTime() +"-"+ filename);
+    	fstream.on('open',function(fd){
+	    	file.pipe(fstream);
+	        fstream.on('close', function () {    
+	            console.log("Upload Finished of " + filename);              
+	            res.send({"successful":true});           //where to go next
+	        });   
+        });     
+    });
+});
+
 var server = http.createServer(app).listen(3000, function () {
 	console.log('server is listenning on localhost:3000');
 });
+
+
